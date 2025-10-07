449b3e8a10d6f51db866db2d0340c1e5fefcad31
Jelena Uscelemova <jelena.uscelemova@aaro.com>
2025-10-07 23:07:12 +0300

    4rd  commit: TC_0003

--- src/test/java/tests/TC_0003_AddNewStudentPostTest.java
+++ src/test/java/tests/TC_0003_AddNewStudentPostTest.java
@@ -6,22 +6,23 @@ import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TC_0003_Add_New_Student_POST {
+public class TC_0003_AddNewStudentPostTest {
 
-    String postEndPoint = "http://thetestingworldapi.com/api/studentsDetails";
+    String postEndPoint = "https://thetestingworldapi.com/api/studentsDetails";
     @Test
     public void addNewStudentPostTest(){
 
         // Pre-requisite
         RequestSpecification postRequest = RestAssured.given();
-        postRequest.body("{\"id\":1,\"first_name\":\"Lena\",\"middle_name\":\"sample string 3\",\"last_name\":\"sample string 4\",\"date_of_birth\":\"sample string 5\"}");
-        postRequest.header("content-type", "application/json, text/json");
+        postRequest.body("{\"id\":1,\"first_name\":\"Lena\",\"middle_name\":\"Vladimirovna\",\"last_name\":\"Usc\",\"date_of_birth\":\"04/11/2001\"}");
+        postRequest.header("content-type", "application/json");
+
 
 
         Response postresponse = postRequest.post(postEndPoint);
         System.out.println(postresponse.statusCode());
         Assert.assertEquals(postresponse.statusCode(),201);
-        Assert.assertEquals(postresponse.jsonPath().getString("data.first_name"), "Jelena_01");
+        Assert.assertEquals(postresponse.jsonPath().getString("first_name"), "Lena");
         System.out.println(postresponse.asPrettyString());
 
 
